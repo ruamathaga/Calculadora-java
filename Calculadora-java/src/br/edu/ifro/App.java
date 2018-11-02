aa195f15cbadcbb70f333a8d0ce15379e710f939
Marcos <Ruama>
2018-11-02 16:16:49 -0300

    terminando Calculadora

--- Calculadora-java/src/br/edu/ifro/App.java
+++ Calculadora-java/src/br/edu/ifro/App.java
@@ -7,18 +7,14 @@ package br.edu.ifro;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
  *
- * @author 04046001232
+ * @author Ruama
  */
 public class App extends Application {
     
